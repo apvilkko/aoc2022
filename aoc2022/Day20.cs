7fe600471f015d81b8d064c698141e6e596aa09c
Antti Pekka Vilkko <antti.pekka.vilkko@gofore.com>
2022-12-21 15:30:12 +0200

    day20 parts 1 & 2

--- aoc2022/Day20.cs
+++ aoc2022/Day20.cs
@@ -1,80 +1,181 @@
 ﻿namespace aoc2022;
-using System.Collections.Generic;
 
 public class Day20 : IAocRunner
 {
-    public override Task Run(string variant)
-    {
-        var input = File.ReadAllText($"day20/{variant}.txt")
-            .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
-            .Select((x, i) => (int.Parse(x), i))
-            .ToList();
+    public class LinkedNode {
+        public long Value { get; set; }
+        public int OriginalIndex { get; set; }
+        public LinkedNode? Prev { get; set; } = null;
+        public LinkedNode? Next { get; set; } = null;
+        public LinkedNode(long value, int index) {
+            Value = value;
+            OriginalIndex = index;
+        }
+        public LinkedNode(LinkedNode other) {
+            Value = other.Value;
+            OriginalIndex = other.OriginalIndex;
+            Prev = other.Prev;
+            Next = other.Next;
+        }
+        public override string ToString()
+        {
+            return $"[{Value} ({OriginalIndex})]";
+        }
+        public bool Is(LinkedNode other) =>
+            other.Value == Value && other.OriginalIndex == OriginalIndex;
+    }
 
-        var ll = new LinkedList<(int, int)>(input);
+    private static void Print(LinkedNode start) {
+        var n = start;
+        do {
+            Console.Write($"{n.Value} ");
+            n = n.Next;
+        } while (n != start);
+        Console.Write("\n");
+    }
 
-        var size = input.Count;
-
-        for (var i = 0; i < size; ++i) {
-            var item = ll.First;
-            int currentIndex = item.Value.Item2;
-            while (currentIndex != i) {
-                item = item.Next;
-                currentIndex = item.Value.Item2;
+    private LinkedNode CreateList(List<(long, int)> input) {
+        LinkedNode? root = null;
+        LinkedNode? prev = null;
+        for (var i = 0; i < input.Count; ++i) {
+            var node = new LinkedNode(input[i].Item1, input[i].Item2);
+            if (i == 0) {
+                root = node;
             }
-            //Console.WriteLine($"{currentIndex} {item.Value}");
-            var current = item;
-            var amount = item.Value.Item1;
+            if (prev != null) {
+                node.Prev = prev;
+                node.Prev.Next = node;
+            }
+            prev = node;
+        }
+
+        root.Prev = prev;
+        root.Prev.Next = root;
+
+        return root;
+    }
+
+    public LinkedNode Mix(LinkedNode root, int size) {
+        //Print(root);
+        for (var i = 0; i < size; ++i) {
+            var c = root;
+            int currentIndex = c.OriginalIndex;
+            while (currentIndex != i) {
+                c = c.Next;
+                currentIndex = c.OriginalIndex;
+            }
+            //Console.WriteLine($"processing {c}");
+            var amount = c.Value;
             if (amount == 0) {
                 continue;
             }
             var backwards = amount < 0;
-            var repeats = Math.Abs(amount) % size;
+            var repeats = Math.Abs(amount) % (size-1);
             for (var j = 0; j < repeats; ++j) {
+                var prev = c.Prev;
+                var next = c.Next;
+                var curr = c;
+                //Console.WriteLine($"  -- {c} p {prev} n {next}");
+                int newRootIndex = -1;
+
+                if (curr.Is(root)) {
+                    //Console.WriteLine("root 1");
+                    newRootIndex = c.Next.OriginalIndex;
+                }
+
                 if (backwards) {
-                    item = item.Previous;
-                    if (item == null) {
-                        //Console.WriteLine("  -- wrapped to last");
-                        item = ll.Last;
+                    if (prev.Is(root)) {
+                        //Console.WriteLine("root prev");
+                        newRootIndex = curr.Prev.OriginalIndex;
                     }
+
+                    next.Prev = prev;
+                    c.Next = prev;
+                    prev.Next = next;
+                    prev.Prev.Next = c;
+                    c.Prev = prev.Prev;
+                    prev.Prev = c;
+
+                    //Console.WriteLine($"  back {c.Prev.Prev} <- {c.Prev} <- {c} -> {c.Next} -> {c.Next.Next}");
                 } else {
-                    item = item.Next;
-                    if (item == null) {
-                        //Console.WriteLine("  -- wrapped to first");
-                        item = ll.First;
+                    if (next.Next.Is(root)) {
+                        //Console.WriteLine("root next");
+                        newRootIndex = curr.OriginalIndex;
                     }
+
+                    next.Prev = prev;
+                    c.Next = next.Next;
+                    prev.Next = next;
+                    next.Next.Prev = c;
+                    c.Prev = next;
+                    next.Next = c;
+
+                    //Console.WriteLine($"  forw {c.Prev.Prev} <- {c.Prev} <- {c} -> {c.Next} -> {c.Next.Next}");
+                }
+                if (newRootIndex > -1) {
+                    var nn = root;
+                    int k = nn.OriginalIndex;
+                    while (k != newRootIndex) {
+                        nn = nn.Next;
+                        k = nn.OriginalIndex;
+                    }
+                    //Console.WriteLine($"  *** setting new root to index {newRootIndex} {nn}");
+                    root = nn;
                 }
             }
-
-            var value = current.Value;
-            //Console.WriteLine($"removing {current.Value}");
-            ll.Remove(current);
-
-            //Console.WriteLine($"adding {value} {(backwards ? "before" : "after")} {item.Value}");
-            if (backwards) {
-                ll.AddBefore(item, value);
-                // If it got placed first, it wraps to last
-                if (ll.First.Value == value) {
-                    ll.RemoveFirst();
-                    ll.AddLast(value);
-                }
-            } else {
-                ll.AddAfter(item, value);
-            }
-
-            //Console.WriteLine($"  {string.Join(",", ll.Select(x => x.Item1).ToList())}");
-            //Console.WriteLine("");
+            //Print(root);
         }
+        return root;
+    }
 
-        var resultList = new List<(int, int)>(ll);
-        var zeroIndex = resultList.FindIndex(x => x.Item1 == 0);
-        var part1 = new List<int>(){1000,2000,3000}.Select(i => {
-            var val = resultList[(zeroIndex + i) % resultList.Count].Item1;
-            Console.WriteLine($"{val}");
-            return val;
-        }).Sum();
-        Console.WriteLine($"part1 {part1}");
+    private long GetCoord(LinkedNode root) {
+        var n = root;
+        var l = n.Value;
+        while (l != 0) {
+            n = n.Next;
+            l = n.Value;
+        }
+        long sum = 0;
+        for (var i = 1; i <= 3000; ++i) {
+            n = n.Next;
+            if (i == 1000 || i == 2000 || i == 3000) {
+                Console.WriteLine($"{i} {n}");
+                sum += n.Value;
+            }
+        }
+        return sum;
+    }
 
-        // 2785 too low
+    public override Task Run(string variant)
+    {
+        var input = File.ReadAllText($"day20/{variant}.txt")
+            .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select((x, i) => (long.Parse(x), i))
+            .ToList();
+
+        var root = CreateList(input);
+        var size = input.Count;
+
+        root = Mix(root, size);
+        var sum = GetCoord(root);
+
+        Console.WriteLine($"part 1: {sum}");
+
+        // part 2
+        var key = 811589153;
+        var input2 = File.ReadAllText($"day20/{variant}.txt")
+            .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select((x, i) => (long.Parse(x) * key, i))
+            .ToList();
+        root = CreateList(input2);
+
+        for (var i = 0; i < 10; ++i) {
+            root = Mix(root, size);
+            Print(root);
+        }
+        sum = GetCoord(root);
+
+        Console.WriteLine($"part 2: {sum}");
 
         return Task.CompletedTask;
     }
